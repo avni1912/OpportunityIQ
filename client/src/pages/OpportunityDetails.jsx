c565e00a2bf07d7aca198d38febbb00394bbd1fd
Avni <avnirohila8127@gmail.com>
2026-07-01 19:31:05 +0530

    Enhanced opportunity details page with deadline status

--- client/src/pages/OpportunityDetails.jsx
+++ client/src/pages/OpportunityDetails.jsx
@@ -1,10 +1,45 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 function OpportunityDetails() {
 
   const { id } = useParams();
+  const navigate = useNavigate();
+  const getDeadlineStatus = (deadline) => {
+
+    const today = new Date();
+    const endDate = new Date(deadline);
+
+    const diffTime = endDate - today;
+    const daysLeft = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (daysLeft < 0) {
+        return {
+            text: "Expired",
+            color: "bg-red-500"
+        };
+    }
+
+    if (daysLeft <= 3) {
+        return {
+            text: "Closing Soon",
+            color: "bg-orange-500"
+        };
+    }
+
+    if (daysLeft <= 7) {
+        return {
+            text: "Closing This Week",
+            color: "bg-yellow-500"
+        };
+    }
+
+    return {
+        text: "Open",
+        color: "bg-green-500"
+    };
+};
   const [opportunity, setOpportunity] = useState(null);
 
   useEffect(() => {
@@ -23,10 +58,19 @@ function OpportunityDetails() {
     return <h2 className="text-center mt-10">Loading...</h2>;
   }
 
+  const status = getDeadlineStatus(opportunity.deadline);
+
  return (
   <div className="min-h-screen bg-gray-100 py-10">
 
     <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-xl p-8">
+
+      <button
+        onClick={() => navigate("/")}
+        className="mb-6 bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-lg"
+      >
+        ← Back to Opportunities
+      </button>
 
       <h1 className="text-4xl font-bold">
         {opportunity.title}
@@ -36,9 +80,19 @@ function OpportunityDetails() {
         {opportunity.organization}
       </p>
 
-      <span className="inline-block mt-4 bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
+      <div className="flex gap-3 mt-4">
+
+    <span className="bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
         {opportunity.category}
-      </span>
+    </span>
+
+    <span
+        className={`${status.color} text-white px-3 py-1 rounded-full`}
+    >
+        {status.text}
+    </span>
+
+</div>
 
       <hr className="my-6" />
 
